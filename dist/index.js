9a0f373dd52ed1944783460a633ec32a9b1159ca
Vinnie Pauley <vinniepauley@gmail.com>
2022-08-03 21:36:05 -0500

    use process env

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const data_1 = __importDefault(require("./data"));
 const app = (0, express_1.default)();
-const port = 3000;
+const { PORT } = process.env;
 app.get('/', (req, res) => {
     res.send(JSON.stringify(data_1.default));
 });
-app.listen(port, () => {
-    console.log('server is runnning on port 3000');
+app.listen(PORT, () => {
+    console.log(`server is runnning on port: ${PORT}`);
 });
--- index.ts
+++ index.ts
@@ -1,14 +1,26 @@
 import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT?: string;
+    }
+  }
+}
 
 import data from './data';
 
 const app: Express = express();
-const port: number = 3000;
+
+const { PORT } = process.env;
 
 app.get('/', (req: Request, res: Response) => {
   res.send(JSON.stringify(data));
 });
 
-app.listen(port, () => {
-  console.log('server is runnning on port 3000');
+app.listen(PORT, () => {
+  console.log(`server is runnning on port: ${PORT}`);
 });
